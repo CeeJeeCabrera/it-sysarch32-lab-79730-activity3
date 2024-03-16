e0713fb20423443dbc1528dca89c145e15afb62d
CeeJeeCabrera <cjcabrera1114@gmail.com>
2024-03-16 21:09:56 +0800

    Add my file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// App.jsx
+
+import React from "react";
+import Header from "./Header";
+import Pokedex from "./Pokedex"
+import Card from "./Card";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Pokedex />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+// Card.jsx
+
+import React from "react";
+import "./index.css";
+
+const Card = ({ children }) => {
+  return <div className="card">{children}</div>;
+};
+
+export default Card;
